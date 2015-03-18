2dcdf845c529452bb5ada0fbb68a0a1e2954b193
markvovo <markv@trixsolutions.com>
2015-03-18 21:26:47 +1000

    Attempted background pic

--- src/button_click.c
+++ src/button_click.c
@@ -2,17 +2,22 @@
 
 static Window *window;
 static TextLayer *text_layer;
+//static BitmapLayer *s_image_layer;
+//static GBitmap *s_image_bitmap;
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Exterminate");
+	//call hhtp post to play sound one
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(text_layer, "Destroy");
+	//call hhtp post to play sound two
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Meow");
+  text_layer_set_text(text_layer, "You may proceed");
+	//call hhtp post to play sound three
 }
 
 static void click_config_provider(void *context) {
@@ -23,15 +28,26 @@ static void click_config_provider(void *context) {
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+ GRect bounds = layer_get_bounds(window_layer);
+ // s_image_bitmap = gbitmap_create_with_resource(dalek_background);
 
+//  s_image_layer = bitmap_layer_create(bounds);
+//  bitmap_layer_set_bitmap(s_image_layer, s_image_bitmap);
+//  bitmap_layer_set_alignment(s_image_layer, GAlignCenter);
+//  layer_add_child(window_layer, bitmap_layer_get_layer(s_image_layer));
+	
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Darlek Control");
+  //text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { 70, 120 } });
+  text_layer_set_text(text_layer, "Dalek Control");
+  //text_layer_set_font(&textLayer, fonts_get_system_font(FONT_KEY_GOTHAM_30_BLACK));
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 static void window_unload(Window *window) {
+ // bitmap_layer_destroy(s_image_layer);
+ // gbitmap_destroy(s_image_bitmap);
+
   text_layer_destroy(text_layer);
 }
 
